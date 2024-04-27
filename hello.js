2d04085e9335277aad6de32d601bcce157b2f745
saurabhain <saurabha.in@gmail.com>
2024-04-27 11:20:09 +0530

    this is a test

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Saurabh as a part of MIT PE course on DevOps!";
 console.log(hello());
 exports.hello = hello;
 
